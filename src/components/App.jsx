58e786f7995609de1ff5c967d8d750ce8ffd0f38
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-06-20 17:55:51 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/:moviId" element={<MovieDetails />} />
+        <Route path="/:moviId/cast" element={<Cast />} />
+        <Route path="/:moviId/reviews" element={<Reviews />} />
+      </Routes>
     </div>
   );
 };
